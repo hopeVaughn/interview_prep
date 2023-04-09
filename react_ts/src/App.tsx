6417bdc565a5aa02435c6a5ff65474a9f2c64f79
Hope <hopevaughnwarren@gmail.com>
2023-04-09 11:33:39 -0700

    update styling for UserTable

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,9 +1,20 @@
-import ToDoList from './solutions/ToDoList';
+import UserTable from './solutions/UserTable';
+interface User {
+  name: string;
+  age: number;
+  gender: 'male' | 'female';
+}
 
+const users: User[] = [
+  { name: 'Alice', age: 25, gender: 'female' },
+  { name: 'Bob', age: 30, gender: 'male' },
+  { name: 'Charlie', age: 20, gender: 'male' },
+  { name: 'Danielle', age: 35, gender: 'female' },
+];
 function App() {
   return (
     <div>
-      <ToDoList />
+      <UserTable users={users} />
     </div>
   );
 }
--- react_ts/src/solutions/UserTable.tsx
+++ react_ts/src/solutions/UserTable.tsx
@@ -3,7 +3,6 @@ In this solution, we define a functional component called UserTable that takes a
 
 We then sort the users array using the sortBy and sortOrder state variables, and render the sorted users in a table. Each column header has an onClick handler
 */
-
 import React, { useState } from 'react';
 
 interface User {
@@ -37,24 +36,44 @@ const UserTable: React.FC<{ users: User[] }> = ({ users }) => {
   });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th onClick={() => handleSort('name')}>Name</th>
-          <th onClick={() => handleSort('age')}>Age</th>
-          <th onClick={() => handleSort('gender')}>Gender</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedUsers.map((user) => (
-          <tr key={user.name}>
-            <td>{user.name}</td>
-            <td>{user.age}</td>
-            <td>{user.gender}</td>
+    <div className='overflow-x-auto'>
+      <table className='w-full table-auto text-left'>
+        <thead>
+          <tr className='bg-gray-100'>
+            <th
+              onClick={() => handleSort('name')}
+              className='px-4 py-2 cursor-pointer hover:bg-gray-200'
+            >
+              Name
+            </th>
+            <th
+              onClick={() => handleSort('age')}
+              className='px-4 py-2 cursor-pointer hover:bg-gray-200'
+            >
+              Age
+            </th>
+            <th
+              onClick={() => handleSort('gender')}
+              className='px-4 py-2 cursor-pointer hover:bg-gray-200'
+            >
+              Gender
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortedUsers.map((user, index) => (
+            <tr
+              key={user.name}
+              className={index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}
+            >
+              <td className='border px-4 py-2'>{user.name}</td>
+              <td className='border px-4 py-2'>{user.age}</td>
+              <td className='border px-4 py-2'>{user.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 

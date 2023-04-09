2e88b1eb81b100fd117a62a9d08de0ff6c0b240d
Hope <hopevaughnwarren@gmail.com>
2023-04-09 09:26:17 -0700

    update comments to reflect changes

--- react_ts/src/solutions/CountUpTimer.tsx
+++ react_ts/src/solutions/CountUpTimer.tsx
@@ -1,13 +1,13 @@
 /*
-In this solution, we define a functional component called Timer that contains three state variables, time, isActive, and isPaused. We use the useState hook to update these state variables when the timer is started, paused, or reset.
+In this solution, we define a functional component called CountUpTimer that contains three state variables, time, isActive, and isPaused. We use the useState hook to update these state variables when the timer is started, paused, or reset.
 
 We also define a intervalRef variable using the useRef hook to store a reference to the interval used for updating the timer.
 
 We define three functions, startTimer, pauseTimer, and resetTimer, that are called when the user clicks the corresponding buttons. Inside these functions, we update the time, isActive, and isPaused state variables, and start, pause, or reset the timer interval using setInterval and clearInterval.
 
-We then render a p element that displays the timer in minutes and seconds. We use the padStart method to add leading zeros to the minutes and seconds when necessary.
+We conditionally render the start, pause, reset, and restart buttons based on the isActive and isPaused state variables. The start button is only displayed when the timer is not active, the pause button is only displayed when the timer is active but not paused, the reset button is displayed when the timer is active or paused, and the restart button is displayed when the timer is paused.
 
-We also conditionally render the start, pause, and reset buttons based on the isActive and isPaused state variables. The start button is only displayed when the timer is not active, the pause button is only displayed when the timer is active but not paused, and the reset button is displayed when the timer is active or paused.
+We then render a p element that displays the timer in minutes and seconds. We use the padStart method to add leading zeros to the minutes and seconds when necessary.
 */
 import React, { useState, useRef } from 'react';
 

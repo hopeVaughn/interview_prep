96c470a63dc01a72fe6ba0cce451d9f65997c1c2
Hope <hopevaughnwarren@gmail.com>
2023-04-09 11:34:03 -0700

    clear App. Done testing.

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,22 +1,5 @@
-import UserTable from './solutions/UserTable';
-interface User {
-  name: string;
-  age: number;
-  gender: 'male' | 'female';
-}
-
-const users: User[] = [
-  { name: 'Alice', age: 25, gender: 'female' },
-  { name: 'Bob', age: 30, gender: 'male' },
-  { name: 'Charlie', age: 20, gender: 'male' },
-  { name: 'Danielle', age: 35, gender: 'female' },
-];
 function App() {
-  return (
-    <div>
-      <UserTable users={users} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

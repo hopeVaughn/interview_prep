73cfdcab04977d1e81dc5a7cd726564e1e0e5d5b
Hope <hopevaughnwarren@gmail.com>
2023-04-09 10:09:25 -0700

    update app for testing

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,16 +1,5 @@
-import React from 'react';
-import DragAndDrop from './solutions/DragAndDrop';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
 function App() {
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div>
-        <DragAndDrop />
-      </div>
-    </DndProvider>
-  );
+  return <div></div>;
 }
 
 export default App;

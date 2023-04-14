22c7582ffcf824f848eb6810a6715488ee6523cf
Anhle3698 <anhviet.le00@hotmail.com>
2023-04-13 21:19:16 -0600

    Change promplem statement

--- react_ts/src/challenges/BodyCardWrap.tsx
+++ react_ts/src/challenges/BodyCardWrap.tsx
@@ -4,7 +4,7 @@
 
 // Requirements:
 
-// The application should have a user-friendly interface for creating and managing content cards resembling the "bodyCardWrap" component.
+// The application should have a user-friendly interface for creating and managing content cards resembling the "BodyCarWrap" component.
 
 // Users should be able to create new cards by inputting the following information:
 // a. Header image (either by providing a URL or uploading an image file)
@@ -16,10 +16,8 @@
 
 // Users should be able to edit and delete existing content cards.
 
-// The application should store and retrieve the created cards using a persistent data storage solution.
-
 // The layout of the content cards should be responsive, adjusting automatically to different screen sizes and resolutions.
 
 // The user interface should have a modern and professional design.
 
-// The main goal of this project is to create an application that allows users to easily create and manage content cards resembling the "bodyCardWrap" component while ensuring a visually appealing and intuitive user experience. The application should be built using modern web technologies such as React, TypeScript, and Tailwind CSS, ensuring maintainability and scalability.
+// The main goal of this project is to create an application that allows users to easily create and manage content cards resembling the "BodyCardWrap" component while ensuring a visually appealing and intuitive user experience. The application should be built using modern web technologies such as React, TypeScript, and Tailwind CSS, ensuring maintainability and scalability.

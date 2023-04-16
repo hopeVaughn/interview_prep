70c18795194d5b42ce626e040fa01fd5a8604f91
hopeVaughn <hopevaughnwarren@gmail.com>
2023-04-16 08:27:48 -0700

    small change for white space

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -1,10 +1,11 @@
 // import React from "react";
 // import LoginForm from "./solutions/LoginForm";
 // import CountdownTimer from "./solutions/CountdownTimer";
-// import BodyCardWrap from "./solutions/BodyCardWrap";
+import BodyCardWrap from "./solutions/BodyCardWrap";
 function App() {
-  let startTime: number = 100;
+  // let startTime: number = 100;
   return <div>
+    <BodyCardWrap />
     {/* <LoginForm />
     <CountdownTimer startTime={startTime} />
     <BodyCardWrap /> */}
